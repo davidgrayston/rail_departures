7141a07c45270d6e02dd1269812e75e7b8bae15d
David Grayston <davidgrayston@googlemail.com>
2016-02-27 23:51:01 +0000

    Initial commit

---
+++ src/departures.c
@@ -0,0 +1,79 @@
+#include <pebble.h>
+
+static Window *window;
+static TextLayer *text_layer;
+static ScrollLayer *scroll_layer;
+
+static ScrollLayer *s_scroll_layer;
+static TextLayer *s_text_layer;
+
+#define apidata 0
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Store incoming information
+  static char departure_buffer[400];
+
+  // Read tuples for data
+  Tuple *departure_tuple = dict_find(iterator, apidata);
+
+  // If all data is available, use it
+  if(departure_tuple) {
+    snprintf(departure_buffer, sizeof(departure_buffer), "%s", departure_tuple->value->cstring);
+  }
+
+  text_layer_set_text(s_text_layer, departure_buffer);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  scroll_layer_set_content_size(s_scroll_layer, text_layer_get_content_size(s_text_layer));
+}
+
+static void handle_init() {
+  Window *window = window_create();
+  Layer *window_layer = window_get_root_layer(window);
+  //window_set_background_color(window, GColorFromRGB(0, 0, 85));
+  GRect bounds = layer_get_bounds(window_layer);
+
+  s_scroll_layer = scroll_layer_create(bounds);
+  scroll_layer_set_click_config_onto_window(s_scroll_layer, window);
+  layer_add_child(window_layer, scroll_layer_get_layer(s_scroll_layer));
+
+  s_text_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, bounds.size.w, 2000));
+  text_layer_set_text(s_text_layer, "\nLoading");
+  text_layer_set_background_color(s_text_layer, GColorFromRGB(0, 0, 85));
+  text_layer_set_text_color(s_text_layer, GColorFromRGB(255, 255, 255));
+  text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  scroll_layer_add_child(s_scroll_layer, text_layer_get_layer(s_text_layer));
+
+  scroll_layer_set_content_size(s_scroll_layer, text_layer_get_content_size(s_text_layer));
+
+  // Must be after added to the view hierachy
+  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 2);
+
+  // Enable ScrollLayer paging
+  scroll_layer_set_paging(s_scroll_layer, true);
+
+  window_stack_push(window, true);
+
+  // Register callbacks
+  app_message_register_inbox_received(inbox_received_callback);
+
+  // Open AppMessage
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+}
+
+void handle_deinit(void) {
+  // Destroy the text layer
+  text_layer_destroy(text_layer);
+
+  // Destroy scroll layer
+  scroll_layer_destroy(scroll_layer);
+
+  // Destroy the window
+  window_destroy(window);
+}
+
+int main(void) {
+  handle_init();
+  app_event_loop();
+  handle_deinit();
+}
---
+++ src/js/app.js
@@ -0,0 +1,129 @@
+/**
+ * Perform XMLHttpRequest.
+ */
+var xhrRequest = function (url, type, body, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+    callback(this.responseText);
+  };
+  xhr.open(type, url);
+  xhr.setRequestHeader('Content-Type', 'text/xml;charset=UTF-8');
+  xhr.send(body);
+};
+
+/**
+ * Get XML element value.
+ *
+ * Using RegExp on responseText, as responseXML isn't supported.
+ */
+var getXmlValue = function(name, xml) {
+  var regex = new RegExp('<' + name + '>(.*?)</' + name + '>', 'g');  
+  var matches = xml.match(regex);
+  var values = [];
+  for (i in matches) {
+    var value = matches[i].replace(regex, '$1');
+    values.push(value);
+  }
+  return values;
+};
+
+/**
+ * Get departure board.
+ */
+function getArrivals() {
+  // SOAP URL.
+  var url = "https://lite.realtime.nationalrail.co.uk/OpenLDBWS/ldb7.asmx";
+
+  // Soap XML.
+  var body = '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:typ="http://thalesgroup.com/RTTI/2013-11-28/Token/types" xmlns:ldb="http://thalesgroup.com/RTTI/2015-05-14/ldb/">\
+  <soapenv:Header>\
+      <typ:AccessToken>\
+         <typ:TokenValue>' + localStorage.getItem('apiKey') + '</typ:TokenValue>\
+      </typ:AccessToken>\
+   </soapenv:Header>\
+   <soapenv:Body>\
+      <ldb:GetDepartureBoardRequest>\
+         <ldb:numRows>10</ldb:numRows>\
+         <ldb:crs>' + localStorage.getItem('station') + '</ldb:crs>\
+      </ldb:GetDepartureBoardRequest>\
+   </soapenv:Body>\
+  </soapenv:Envelope>';
+
+  // Get arrivals from NRE.
+  xhrRequest(url, 'POST', body, function(responseText) {
+
+    // Get service list.
+    var services = getXmlValue('lt3:service', responseText);
+
+    // Create list of departures.
+    var list = [];
+
+    for (i in services) {
+      // Get service item.
+      var service = services[i];
+
+      // Create array of parts to be joined with comma.
+      var text = [];
+
+      // Destination station.
+      var destination = getXmlValue('lt3:destination', service);
+      var station = getXmlValue('lt2:locationName', destination[0]);
+      // Add via if available.
+      var via = getXmlValue('lt3:via', service);
+      if (via.length > 0) {
+        station += ' ' + via;
+      }
+      text.push(station);
+
+      // Time.
+      var std = getXmlValue('lt3:std', service);
+      std += ' (' + getXmlValue('lt3:etd', service) + ')';
+      text.push(std);
+
+      // Platform.
+      var plat = getXmlValue('lt3:platform', service);
+      if (plat.length > 0) {
+        text.push('Plat:' + plat); 
+      }
+
+      // Add to board list.
+      list.push(text.join(', '));
+    }
+
+    // Send to Pebble
+    Pebble.sendAppMessage({
+      'apidata':list.join("\n\n")
+    });
+  });
+}
+
+/**
+ * Show config.
+ */
+Pebble.addEventListener('showConfiguration', function() {
+  var url = 'https://rawgit.com/davidgrayston/rail_departures/master/config/index.html';
+  Pebble.openURL(url);
+});
+
+/**
+ * Save config.
+ */
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  localStorage.setItem('station', configData['station']);
+  localStorage.setItem('apiKey', configData['apiKey']);
+});
+
+/**
+ * Get departures when app is opened.
+ */
+Pebble.addEventListener('ready', function(e) {
+  getArrivals();
+});
+
+/**
+ * Get departures when an AppMessage is received.
+ */
+Pebble.addEventListener('appmessage', function(e) {
+  getArrivals();
+});
