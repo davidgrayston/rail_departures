456c38b7a9427a840e2519973dc85ca7066dae14
David Grayston <davidgrayston@googlemail.com>
2016-02-28 20:43:37 +0000

    Powered by loading screen

--- src/departures.c
+++ src/departures.c
@@ -1,9 +1,8 @@
 #include <pebble.h>
 
 static Window *window;
+static Layer *window_layer;
 static TextLayer *text_layer;
-static ScrollLayer *scroll_layer;
-
 static ScrollLayer *s_scroll_layer;
 static TextLayer *s_text_layer;
 
@@ -27,44 +26,46 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     snprintf(departure_buffer, sizeof(departure_buffer), "%s", departure_tuple->value->cstring);
   }
 
-  text_layer_set_text(s_text_layer, departure_buffer);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
-  scroll_layer_set_content_size(s_scroll_layer, text_layer_get_content_size(s_text_layer));
-}
-
-static void handle_init() {
-  Window *window = window_create();
-  Layer *window_layer = window_get_root_layer(window);
+  // Get window bounds.
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create scroll layer.
   s_scroll_layer = scroll_layer_create(bounds);
   scroll_layer_set_click_config_onto_window(s_scroll_layer, window);
+  scroll_layer_set_paging(s_scroll_layer, true);
   layer_add_child(window_layer, scroll_layer_get_layer(s_scroll_layer));
 
-  // Show loading screen.
+  // Create and populate text layer.
   s_text_layer = text_layer_create(GRect(bounds.origin.x, bounds.origin.y, bounds.size.w, 2000));
-  text_layer_set_text(s_text_layer, "\nLoading");
-  text_layer_set_background_color(s_text_layer, GColorFromRGB(0, 0, 85));
   text_layer_set_text_color(s_text_layer, GColorFromRGB(255, 255, 255));
+  text_layer_set_background_color(s_text_layer, GColorFromRGB(0, 0, 85));
   text_layer_set_text_alignment(s_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_text(s_text_layer, departure_buffer);
+
+  // Add text to scroll layer.
   scroll_layer_add_child(s_scroll_layer, text_layer_get_layer(s_text_layer));
   scroll_layer_set_content_size(s_scroll_layer, text_layer_get_content_size(s_text_layer));
 
+  // Enable text flow.
+  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 5);
+}
+
+static void handle_init() {
+  // Setup window layer.
+  window = window_create();
+  window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  window_set_background_color(window, GColorFromRGB(0, 0, 85));
+  window_stack_push(window, true);
+
   // Display app image.
-  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_MENU_ICON);
+  s_bitmap = gbitmap_create_with_resource(RESOURCE_ID_POWERED_BY);
   s_bitmap_layer = bitmap_layer_create(bounds);
   bitmap_layer_set_bitmap(s_bitmap_layer, s_bitmap);
   bitmap_layer_set_compositing_mode(s_bitmap_layer, GCompOpSet);
+  bitmap_layer_set_background_color(s_bitmap_layer, GColorFromRGB(255, 255, 255));
   layer_add_child(window_layer, bitmap_layer_get_layer(s_bitmap_layer));
-
-  // Must be after added to the view hierachy
-  text_layer_enable_screen_text_flow_and_paging(s_text_layer, 2);
-
-  // Enable ScrollLayer paging
-  scroll_layer_set_paging(s_scroll_layer, true);
-
-  window_stack_push(window, true);
 
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
@@ -78,7 +79,7 @@ void handle_deinit(void) {
   text_layer_destroy(text_layer);
 
   // Destroy scroll layer
-  scroll_layer_destroy(scroll_layer);
+  scroll_layer_destroy(s_scroll_layer);
 
   // Destroy the window
   window_destroy(window);
